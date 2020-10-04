337ddbde763ba5bcd326e0f9351e4c5e896c2de8
Ricardo Vera <ricardoveramedina@gmail.com>
2020-10-05 02:18:54 +0900

    Updates

---
+++ precache-manifest.4c45fa446279b5e329baf028d96a6162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881739a9e90ae8b6e91053eaadcc070c",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "0c8aa85fff117dc909d9",
+    "url": "/Portafolio/static/css/main.11b559cb.chunk.css"
+  },
+  {
+    "revision": "5a0aa3ed7a5889f77d98",
+    "url": "/Portafolio/static/js/2.013ea2b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portafolio/static/js/2.013ea2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8aa85fff117dc909d9",
+    "url": "/Portafolio/static/js/main.ffb7e2e0.chunk.js"
+  },
+  {
+    "revision": "a5166c5545381b6e0a20",
+    "url": "/Portafolio/static/js/runtime-main.c0423103.js"
+  },
+  {
+    "revision": "6bf30b6ebc3c6b3c75070d42c4b289c1",
+    "url": "/Portafolio/static/media/sofiapro-light.6bf30b6e.otf"
+  }
+]);
