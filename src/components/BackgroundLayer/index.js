2238f5e959d5d29974f89f917378af3e058c02ea
Ricardo Vera <ricardoveramedina@gmail.com>
2020-10-09 22:11:48 +0900

    structure updated and some mobile fixed

--- src/components/BackgroundLayer/index.js
+++ src/components/BackgroundLayer/index.js
@@ -5,8 +5,8 @@ export default function BackgroundLayer(props) {
   const { ParallaxLayer } = props;
   return (
     <Fragment>
-      <ParallaxLayer factor={3} offset={2} speed={0.5} className="strato" />
-      <ParallaxLayer factor={3} offset={3} speed={0.5} className="sky" />
+      <ParallaxLayer factor={3} offset={2} speed={0} className="strato" />
+      <ParallaxLayer factor={3} offset={4} speed={0} className="sky" />
     </Fragment>
   );
 }
